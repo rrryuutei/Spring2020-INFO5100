198bcda118b79435ba0abb2738c3cbb92092ac77
rrryuutei <48124924+rrryuutei@users.noreply.github.com>
2022-02-19 16:37:20 -0800

    Update person.java

--- assignment2/src/person.java
+++ assignment2/src/person.java
@@ -16,15 +16,3 @@ class Person {
  
 }
 
-class Professor extends Person{
-
- public Professor(String name, int ID, int age) {
-  super(name, ID, age);
-  // TODO Auto-generated constructor stub
- }
- 
- public String giveQuiz() {
-  return super.toString();
- }
- 
-}
